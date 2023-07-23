38bc541f2230b11bf58422bcc5a6f0f262af430e
aazzam <amandas.azzam@gmail.com>
2023-07-22 16:50:25 -0800

    Add eventlistener to change button text and appearance when clicked

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 var button = document.querySelector(".show-cat");
-button.addEventListener("click", function () {
-  cat.classList.add("show");
-});
-
 var cat = document.querySelector(".cat");
+
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+  }
+});
